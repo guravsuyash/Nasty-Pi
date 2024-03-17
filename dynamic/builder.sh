f047cd44903cc7e3259a9363051998feb0d72da4
guravsuyash <55230261+guravsuyash@users.noreply.github.com>
2024-03-17 23:16:05 +0530

    Update builder.sh

--- dynamic/builder.sh
+++ dynamic/builder.sh
@@ -1,3 +1,20 @@
-mv dbbackup.sql init.sql
+if [ -f dbbackup.sql ]; then
+    mv dbbackup.sql init.sql
+    echo "File renamed successfully"
+else
+    echo "dbbackup.sql does not exist!!!"
+fi
+
+# Check if .env file exists
+if [ -f ".env" ]; then
+    # Perform the replacements using sed
+    sed -i "s/DB_HOST=.*/DB_HOST='db'/" .env
+    sed -i "s/DB_DATABASE=.*/DB_DATABASE='mydb'/" .env
+    sed -i "s/DB_USERNAME=.*/DB_USERNAME='admin'/" .env
+    sed -i "s/DB_PASSWORD=.*/DB_PASSWORD='admin'/" .env
+    echo ".env has been edited."
+else
+    echo "Warning: .env file not found. Continuing without editing .env."
+fi
 docker build . -f DockerMysql -t dynamic-mysql
 docker build . -f Dockerfile -t dynamic
